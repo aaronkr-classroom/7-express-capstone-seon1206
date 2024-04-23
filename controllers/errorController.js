44b5da27f0e7c64d3df2811f697e5ad0ad48b67a
seon1206 <127457404+seon1206@users.noreply.github.com>
2024-04-23 08:13:20 +0000

    12끝!!!!!

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,18 +1,31 @@
 // errorController.js
 "use strict";
-
+const httpStatus = require("http-status-codes");
 
 
 /**
  * Listing 11.2 (p. 168)
  */
-
+exports.logErrors = (err, req, res, next) => {
+    console.error(err.stack);
+    next(err)
+};
 
 /**
  * Listing 12.11 (p. 184)
  * 에러 처리 라우트 추가
  */
-
+exports.pageNotFoundError = (req, res) => {
+    let errorCode = httpStatus.NOT_FOUND;
+    res.status(errorCode);
+    res.render("error");
+}
+exports.internalServerError = (error, req, res, next) => {
+    let errorCode = httpStatus.INTERNAL_SERVER_ERROR;
+    console.log(`ERROR occurred: ${error.stack}`);
+    res.status(errorCode);
+    res.send(`${errorCode} | Sorry, our application is taking a nap!`);
+};
 
 /**
  * [노트] 라우트 순서는 중요하다. 이 라우트는 기존에 존재하는 라우트 아래에 와야 한다.
--- main.js
+++ main.js
@@ -3,29 +3,53 @@
 "use strict";
 
 // 앱 설정
+const express = require("express"),
+app = express(),
+homeController = require("./controllers/homeController"),
+errorController = require("./controllers/errorController");
+
+app.set("port", process.env.PORT || 3000);
+app.set("/", (req, res) => {
+    res.send("Welcome to Confetti Cuisine!");
+});
 
 /**
  * Listing 12.7 (p. 179)
  * ejs 레이아웃 렌더링
  */
+const layouts = require("express-ejs-layouts");
 
-
+app.set("view engine", "ejs");
+app.use(layouts);
 /**
  * Listing 12.4 (p. 177)
  * body-parser의 추가
  */
 
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+);
+app.use(express.json());
 
 /**
  * Listing 12.6 (p. 178)
  * 각 페이지 및 요청 타입을 위한 라우트 추가
  */
-
+app.get("/", homeController.getHome);
+app.get("/courses", homeController.showCourses);
+app.get("/contact", homeController.showSignUp);
+app.post("/contact", homeController.postSignupForm);
 
 /**
  * Listing 12.12 (p. 184)
  * 에러 처리 라우트 
  */
-
+app.use(errorController.pageNotFoundError);
+app.use(errorController.internalServerError);
 
 // 3000번 포트로 리스닝 설정
+app.listen(app.get("port"), () => {
+    console.log(`Server Running at http://localhost:${app.get("port")}`);
+});
